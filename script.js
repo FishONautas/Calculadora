9a7d9d70ba6b6fd5228dd652703e8cb802b79d23
João Victor Garcia <joaovgarcia047@gmail.com>
2023-08-29 17:27:31 -0300

    NovaCalculadora

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-let valorDisplay = ''
-let valoratual = document.getElementById('display')
+let previewOperacao = document.getElementById('operacao-preview')
+let atualOperacao = document.getElementById('operacao-atual')
+// declarando variavel pelo ID
 
-function insert(numero){
-valorDisplay += numero
-document.getElementById('display').value = valorDisplay
-}
+let buttons = document.querySelectorAll("#botoes-container button")
+// declarando variavel através de todo elemento que se identifica como "button" dentro da div #botoes-container, por isso usamos o querySelectorAll
+// o getElementById seleciona apenas um elemento especifico que se indetificar com o ID
+// o querySelector tem a mesma funcionalidade do getElementById, porém ele trás mais possiblidades de requisitarmos o elemento.
+// o querySelectorAll irá capturar todo aquele elemento que se identificar com determinada classe ou ID, caso haja mais de um, ele retornará todos os resultados, como uma lista como estamos fazendo agora para capturar os botoes
 
-function limpar() {
-    valorDisplay = '';
-    document.getElementById('display').value = valorDisplay
-}
 
-function calcular(){
-    if (valoratual.value == ''){
-        document.getElementById('display').value = 'Erro';
-        valorDisplay = '';
-        
-    }
-    else{
-    try {
-        let resultado = FazerConta(valorDisplay);
-        document.getElementById('display').value = resultado;
-        valorDisplay = '';
-    } 
 
-    catch (error) {
-        document.getElementById('display').value = 'Erro';
-        valorDisplay = '';
-        
-    }
-}
-}
-
-function FazerConta(expressao) {
-    return new Function('return ' + expressao)();
-}
